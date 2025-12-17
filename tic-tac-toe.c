5a6bfb8a23b0d36cca6775726b1e36eb106bdc3c
Saad Ahmed <saadahmd7223@gmail.com>
2025-12-17 23:53:41 +0530

    Added input difficulty function

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,7 +1,30 @@
 // Tic Tac Toe game using C
 #include <stdio.h>
 
+int difficulty;
+
+void input_difficulty();
+
 int main(){
-    printf("Welcome to Tic Tac Toe game!");
+    input_difficulty();
+
     return 0;
+}
+
+void input_difficulty(){
+    while(1){
+    printf("\nSelect difficulty level:");
+    printf("\n1. Human (Standard)");
+    printf("\n2. God (Impossible to win)");
+
+    printf("\nEnter your choice: ");
+    scanf("%d", &difficulty);
+
+    if(difficulty != 1 && difficulty != 2){
+        printf("\nIncorrect choice, Enter (1/2)!");
+    }
+    else{
+        break;
+    }
+    } 
 }
